68aa6e186afb59e80b0a25b13f920e22f148a285
Noele Maria <noele.maria.nm@gmail.com>
2024-01-17 17:56:23 -0300

    Colocando saudação

--- 03.js
+++ 03.js
@@ -1,6 +1,7 @@
+let nome = prompt("Olá, Digite seu nome: ")
 function calcularDobro (numero) {
     return numero * 2;
 }
 
 let resultadoDobro = calcularDobro(5);
-console.log(resultadoDobro);
+console.log(`${nome}, o dobro de 5 é ${resultadoDobro}`);
